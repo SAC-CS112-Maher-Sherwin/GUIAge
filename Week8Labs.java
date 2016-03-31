a68fde8beeae2cfad823c9f6ee555c8b6f982a4e
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-03-30 20:08:55 -0700

    Update Week8Labs.java

--- Week8Labs.java
+++ Week8Labs.java
@@ -1,5 +1,3 @@
-
-package week8labs;
 import java.util.Calendar;
 import javax.swing.JOptionPane;
 
@@ -7,29 +5,32 @@ public class Week8Labs {
     
     public static void main(String[] args) {
         
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-       
-        String userName = JOptionPane.showInputDialog("Hello, what is your name?");
-        String userYear = JOptionPane.showInputDialog("In what year were you born?");
-        
-        int birthYear = Integer.parseInt(userYear);
-        
-        int userAge = currentYear - birthYear;
-        
-            if (userAge >= 21){
-                
-                String drink = String.format("Hello, %s, you are %d years old today! "
-                        + "You can buy a drink!", userName, userAge);
-                JOptionPane.showMessageDialog(null, drink);
-                }
+        for (int i = 1; i < 4; i++){
             
-            else {
-                int legalYear = ((21-userAge)+ currentYear);
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+       
+            String userName = JOptionPane.showInputDialog("Hello, what is your name?");
+            String userYear = JOptionPane.showInputDialog("In what year were you born?");
+        
+           int birthYear = Integer.parseInt(userYear);
+            int userAge = currentYear - birthYear;
+        
+                if (userAge >= 21){
                 
-                String noDrink = String.format("Hello, %s, you are %d years old today! "
+                    String drink = String.format("Hello, %s, you are %d years old today! "
+                        + "You can buy a drink!", userName, userAge);
+                    JOptionPane.showMessageDialog(null, drink);
+                    }
+            
+                else {
+                    int legalYear = ((21-userAge)+ currentYear);
+                
+                    String noDrink = String.format("Hello, %s, you are %d years old today! "
                         + "You can drink in %d.", userName, userAge, legalYear);
                 
-                JOptionPane.showMessageDialog(null, noDrink);
+                    JOptionPane.showMessageDialog(null, noDrink);
+                }
+            
             }
        
     }
